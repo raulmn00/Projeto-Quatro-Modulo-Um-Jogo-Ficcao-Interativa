c9f996ac64a51b0ceefca7017b90dc7b23bd2240
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-06-13 21:33:09 -0300

    mudanca do background da historia para um terror de zumbis

--- projeto-4-jogo-ficcao-interativa.js
+++ projeto-4-jogo-ficcao-interativa.js
@@ -1,52 +1,58 @@
 const prompt = require("prompt-sync")();
 console.clear();
 
-const personagem = {
+const protagonista = {
     nome: "",
-    idade: "",
-    objetivo: false,
+    idade: 0,
+    vivo: true,
     fome: false,
-    sono: false,
-    determinacao: 100,
-    dormir: function () {
-        this.determinacao += 10;
-    },
+    sono: false
 };
 
-let contadorHoras = 0;
-
-let periodoManha = true;
-let periodoTarde = true;
-let periodoNoite = true;
-
-while (personagem.objetivo === false) {
-    while(periodoManha === true){
-        console.log(`Passaram ${contadorHoras} horas da manhã.`);
-        contadorHoras++;
-        if(contadorHoras === 8){
-            periodoManha = false;
+let qtdAcoes = 0;
+let contadorDias = 0;
+protagonista.nome = prompt("Digite o nome do protagonista: ");
+protagonista.idade = +prompt("Digite a idade do protagonista: ");
+console.log("Infelizmente é isso. O mundo acabou, os mortos andam pela terra e toda ficção sobre zumbis era uma mentira.");
+console.log("Eles são mais rápidos, mais inteligentes e muito mais fortes do que a humanidade previu, e isso, só acelerou o processo do apocalipse.");
+console.log(`Mas, milagrosamente, algumas pessoas conseguiram sobreviver e você ${protagonista.nome} é uma delas.`);
+console.log("Agora, você precisa continuar vivo o máximo que conseguir...");
+while(protagonista.vivo === true){
+    if(qtdAcoes === 5){
+        protagonista.fome = true;
+        protagonista.sono = true;
+    }
+    console.log(`É o dia ${contadorDias}º, voce acorda e precisa decidir o que fazer hoje!`);
+    console.log(`
+    [1] - Você decide ir atrás de mantimenetos.
+    [2] - Você decide vasculhar o perímetro e ter certeza de sua segurança.
+    [3] - Você decide procurar outros sobreviventes.`);
+    let escolhaDiaria = +prompt("Digite sua escolha: ");
+    if(escolhaDiaria === 1){
+        console.log("Você decidiu ir atrás de mantimentos. Vamos pegar nosso mapa local e procurar.");
+        console.log(`Ao ler o mapa, você encontrou 2 lugares próximos que podem ter mantimentos. Qual local você deseja procurar?
+        [1] - Um supermercado.
+        [2] - Uma farmácia.
+        [3] - Ambos os locais`);
+        let escolhaMantimentos = +prompt("Digite sua escolha: ");
+        if(escolhaMantimentos === 1){
+            qtdAcoes++;
+            console.log("Você decidiu ir atrás do supermercado.");
+        }else if(escolhaMantimentos === 2){
+            qtdAcoes++;
+            console.log("Voce decidiu ir até a farmácia.");
+        }else if(escolhaMantimentos === 3){
+            qtdAcoes += 2;
+            console.log("Você decide procurar em ambos os locais.");
+            console.log("Infelizmente voce se deparou com um grupo enorme de zumbis e eles te mataram.");
+            protagonista.vivo = false;
         }
+    }else if(escolhaDiaria === 2){
+        console.log("Você decidiu vasculhar o perímetro. Vamos olhar ao redor e procurar zumbis.");
+    }else if(escolhaDiaria === 3){
+        console.log("Você decidiu procurar outros sobreviventes. Espero que consigamos achar alguém.");
     }
-    while(periodoTarde === true){
-        console.log(`Passaram ${contadorHoras} horas da tarde.`);
-        contadorHoras++;
-        if(contadorHoras === 16){
-            periodoTarde = false;
-        }
-    }
-    while(periodoNoite === true){
-        console.log(`Passaram ${contadorHoras} horas da noite.`);
-        contadorHoras++;
-        if(contadorHoras === 24){
-            periodoNoite = false;
-        }
-    }
-    let perguntaObjetivo = prompt(
-        "Voce atingiu seu objetivo? (S/N)"
-    ).toUpperCase();
-    if (perguntaObjetivo === "S") {
-        personagem.objetivo = true;
-    } else {
-        console.log("Voce ainda não atingiu seu objetivo...");
-    }
+    contadorDias++;
 }
+
+
